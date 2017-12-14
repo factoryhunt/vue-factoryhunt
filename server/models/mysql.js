1b118bd6401a451e9dd799fc712cf4d4176ac37a
Friday <peterkimzz69@gmail.com>
2017-12-14 17:46:37 +0900

    mysql version changed

--- server/models/mysql.js
+++ server/models/mysql.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 // properties
-const host = 'fh-test-2017-10-14-09-25.cfwdgkhtxdcy.us-west-1.rds.amazonaws.com';
+const host = 'fh-test-2017-10-14-09-25-2017-12-12-09-19.cfwdgkhtxdcy.us-west-1.rds.amazonaws.com';
 const user = 'zerobaseinc';
 const password = 'Ycombinator2017';
 const database = 'fh';
