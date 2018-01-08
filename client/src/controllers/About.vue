8f4edfa9dc6a8d11d036217cb97134e3b76b823c
Friday <peterkimzz69@gmail.com>
2018-01-08 13:54:02 +0900

    Version 1.09.34 built -css hot-fix

--- client/src/controllers/About.vue
+++ client/src/controllers/About.vue
@@ -46,56 +46,21 @@
       eng: {
         title: 'About Factory Hunt',
         description: `
-        <div style="font-size: 22px;font-weight:300">
-          <p>Factory Hunt is a supplier search engine & B2B platform where businesses can be connected each other.</p>
-          <p>Any manufacturer/supplier can create a website and online product catalog for free. Once a website/catalog is created, the information will be displayed in search results. Factory Hunt is the easiest way to showcase products to potential business partners.</p>
-          <p>Our mission is to make a free website for every suppliers in the world and make it easy to connect businesses. As a platform, we will continue to develop services to help businesses and discover new opportunities.</p>
-          <p>Best regards,</p>
-          <p>Factory Hunt Team</p>
-        </div>`
+          <p style="font-size: 22px;font-weight:300">Factory Hunt is a supplier search engine & B2B platform where businesses can be connected each other.</p>
+          <p style="font-size: 22px;font-weight:300">Any manufacturer/supplier can create a website and online product catalog for free. Once a website/catalog is created, the information will be displayed in search results. Factory Hunt is the easiest way to showcase products to potential business partners.</p>
+          <p style="font-size: 22px;font-weight:300">Our mission is to make a free website for every suppliers in the world and make it easy to connect businesses. As a platform, we will continue to develop services to help businesses and discover new opportunities.</p>
+          <p style="font-size: 22px;font-weight:300">Best regards,</p>
+          <p style="font-size: 22px;font-weight:300">Factory Hunt Team</p>`
       },
       kor: {
         title: '팩토리 헌트 소개',
         description: `
-        <div style="font-size: 22px;font-weight:300">
-          <p>팩토리 헌트는 제조업체 전문 검색엔진으로 제조업체와 해외 바이어를 연결하는 비지니스 플랫폼입니다.</p>
-          <p>모든 제조업체는 팩토리 헌트를 통해 무료로 홈페이지와 온라인 카탈로그의 제작이 가능합니다. 기업 정보를 업데이트하고 제품 카탈로그가 완성되면 검색엔진 최적화를 통해 제품 정보가 바이어에게 노출됩니다. 팩토리 헌트는 바이어에게 제품을 알리고 판로개척을 위한 가장 쉬운 방법입니다.</P>
-          <p>우리의 목표는 세계의 모든 제조업체의 정보를 디지털화하고 바이어와의 비즈니스를 쉽게 연결할 수 있도록 하는 것입니다. 플랫폼으로써 우리는 비즈니스를 지원할 것이고 사용자가 새로운 기회를 발견할 수 있도록 서비스를 개발할 것입니다.</p>
-          <p>감사합니다.</p>
-          <p>팩토리 헌트팀</p>
-        </div>`
+          <p style="font-size: 22px;font-weight:300">팩토리 헌트는 제조업체 전문 검색엔진으로 제조업체와 해외 바이어를 연결하는 비지니스 플랫폼입니다.</p>
+          <p style="font-size: 22px;font-weight:300">모든 제조업체는 팩토리 헌트를 통해 무료로 홈페이지와 온라인 카탈로그의 제작이 가능합니다. 기업 정보를 업데이트하고 제품 카탈로그가 완성되면 검색엔진 최적화를 통해 제품 정보가 바이어에게 노출됩니다. 팩토리 헌트는 바이어에게 제품을 알리고 판로개척을 위한 가장 쉬운 방법입니다.</P>
+          <p style="font-size: 22px;font-weight:300">우리의 목표는 세계의 모든 제조업체의 정보를 디지털화하고 바이어와의 비즈니스를 쉽게 연결할 수 있도록 하는 것입니다. 플랫폼으로써 우리는 비즈니스를 지원할 것이고 사용자가 새로운 기회를 발견할 수 있도록 서비스를 개발할 것입니다.</p>
+          <p style="font-size: 22px;font-weight:300">감사합니다.</p>
+          <p style="font-size: 22px;font-weight:300">팩토리 헌트팀</p>`
       }
-    },
-    methods: {
-      getId: function () {
-        return '<h1>{{ id }}</h1>'
-      },
-      requestProducts () {
-        this.$http.get('api/products')
-          .then(res => {
-            this.products = res.data
-          })
-      },
-      onButtonClick () {
-        $('#myButton').modal()
-      },
-      funLoad () {
-        const height = $(window).height()
-        $('.containers').css({'height': height + 'px'})
-      }
-    },
-    created () {
-      window.scrollTo(0, 0)
-//      window.onload = this.funLoad()
-//      window.onresize = this.funLoad()
-      this.requestProducts()
-    },
-    mounted () {
-//      this.funLoad()
-    },
-    // watch는 key값의 변화를 항상 감시한다.
-    watch: {
-      $route: 'getId'
     }
   }
 </script>
